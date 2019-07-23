eca81de367d129d8b5f17865643f7713ef5b9226
isaCarvalho <isabelacarvalhocf@hotmail.com>
2019-07-23 13:30:28 -0300

    Numeracao das linhas

--- js/carregarResultado.js
+++ js/carregarResultado.js
@@ -10,23 +10,29 @@ form.addEventListener("submit", (event) => {
     .then(response => {
         let destino = document.querySelector("pre[name=cdestino]");
         console.log(response);
+        
+        destino.innerHTML = '';
 
         let array = response.prototipo.split('\n');
         console.log(array);
 
         /**Terminar o contador de linhas*/
-        // let lineCounter = 0;
+        let lineCounter = 0;
         array.forEach(line => {
-            // let counter = document.createElement('span');
-            // counter.innerHTML = ++lineCounter;
-            // destino.appendChild(counter);
+            let linha = document.createElement('nav');
+            linha.className = "line";
+
+            let counter = document.createElement('span');
+            counter.className = "number";
+            counter.innerHTML = ++lineCounter;
+            linha.appendChild(counter);
 
             let code = document.createElement('code');
             code.innerHTML =`<span>${line}</span>`;
-            destino.appendChild(code);
+            linha.appendChild(code);
 
-            let br = document.createElement('br');
-            destino.appendChild(br);
+
+            destino.appendChild(linha);
         });
     })
 })
