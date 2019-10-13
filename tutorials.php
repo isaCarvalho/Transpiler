9e5193a546cbad97fca9430c089ca8518dec9ae0
isaCarvalho <isabelacarvalhocf@hotmail.com>
2019-10-13 08:09:40 -0300

    API

--- tutorials.php
+++ tutorials.php
@@ -9,7 +9,10 @@ include "view/header.html";
 <body class="body" id="body" onload="carregarSelect()">
 	
 </body>
-
+<footer class="footer">
+    <p>*Para utilizar a API: http://goto-transpiler.herokuapp.com/control/?action=API&id=ID_LINGUAGEM</p>
+    <p>Substituia o ID_LIGUAGEM por * para retornar todos os dados</p>
+</footer>
 
 <?php
 
