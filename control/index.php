f14c822521565c045eae59c8cc73828534527c7f
isaCarvalho <isabelacarvalhocf@hotmail.com>
2019-11-08 18:00:18 -0300

    Classe Controller/Diagrama de classe atualizado

--- control/index.php
+++ control/index.php
@@ -1,29 +1,39 @@
 <?php
 
-extract($_GET);
-extract($_POST);
-
-switch ($action) 
+class Controller
 {
-	case 'enviarFonte':
+    public function enviarFonte()
+    {
+        $cfonte   = $_GET['cfonte'];
+        $lfonte   = $_GET['lfonte'];
+        $ldestino = $_GET['ldestino'];
+
         require_once "../model/Analise.php";
 
         $result = Analise::analisar($cfonte, $lfonte, $ldestino);
         echo json_encode(["prototipo" => $result]);
-        break;
+    }
 
-    case 'API':
+    public function API()
+    {
+        $id = $_GET['id'];
         require_once "../api/API.php";
 
         echo API::apiLoad($id);
-        break;
+    }
 
-    case 'saveFile':
+    public function saveFile()
+    {
+        $content = $_POST['content'];
+
         require_once '../util/WriteFile.php';
+
         var_dump($_POST);
         echo WriteFile::saveFile('../files/linguagens.json', $content);
-        break;
-
-	default:
-        break;
+    }
 }
+
+extract($_GET);
+
+$c = new Controller();
+$c->$action();
